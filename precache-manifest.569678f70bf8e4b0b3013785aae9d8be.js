3b063a2e41091e7c919b21e66c1a0c2570a02d81
Harjinder Singh <Harjinder.Singh@iontrading.com>
2020-06-01 15:20:28 +0100

    Updates

---
+++ precache-manifest.569678f70bf8e4b0b3013785aae9d8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192665d2c98427dfd4eb298442424f47",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5581bbda900f11eaabc4",
+    "url": "/monsters-rolodex/static/css/main.308788f4.chunk.css"
+  },
+  {
+    "revision": "e088e9077b20b062ebd4",
+    "url": "/monsters-rolodex/static/js/2.440a0972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.440a0972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5581bbda900f11eaabc4",
+    "url": "/monsters-rolodex/static/js/main.f49879f7.chunk.js"
+  },
+  {
+    "revision": "6084ad0c13926bf12fd9",
+    "url": "/monsters-rolodex/static/js/runtime-main.b4aaacc4.js"
+  }
+]);
